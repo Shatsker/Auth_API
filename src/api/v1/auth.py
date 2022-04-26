c5fe23698dd073283f262424f53269a6e274ad4a
Sergey <sergey_shatsker@mail.ru>
2022-04-26 21:36:12 +0300

    Добавил /refresh

--- src/api/v1/auth.py
+++ src/api/v1/auth.py
@@ -1,9 +1,7 @@
-from http import HTTPStatus
+from flask import Blueprint, request
+from flask_jwt_extended import jwt_required, get_jwt
 
-from flask import Blueprint
-from flask import make_response, jsonify, request
-
-from services.auth import AuthService
+from services.auth import LoginService, TokenService
 
 
 auth_router = Blueprint('auth_router', __name__)
@@ -11,17 +9,25 @@ auth_router = Blueprint('auth_router', __name__)
 
 @auth_router.route('/api/v1/login', methods=('POST', ))
 def login_user():
+    """Аутентификация юзера в системе. Обмен логина и пароля на пару jwt токенов."""
     data = request.get_json()
-    tokens = AuthService().login_user(
+
+    return LoginService().login_user(
         login=data['login'],
         password=data['password'],
         user_agent=request.user_agent.string,
     )
 
-    if tokens.get('detail'):
-        return make_response(tokens, HTTPStatus.BAD_REQUEST)
 
-    return make_response(
-        jsonify(tokens),
-        HTTPStatus.OK,
+@auth_router.route('/api/v1/refresh', methods=('POST', ))
+@jwt_required(refresh=True)
+def refresh_tokens():
+    """Обновление токенов юзера. Обмен старого refresh на два свежих jwt токена."""
+    payload_data = get_jwt()
+    old_refresh_token = request.headers.get('Authorization').removeprefix('Bearer ')
+
+    return TokenService().refresh_tokens(
+        sub=payload_data['sub'],
+        additional_claims={'roles': payload_data['roles']},
+        refresh_token=old_refresh_token,
     )
